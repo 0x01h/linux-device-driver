ba3174fbdc64b34b14d2f7926e9499009ca5fd74
Orçun Özdemir <benorcunozdemir@gmail.com>
2018-11-28 20:16:54 +0300

    Add elegant comments and fix style

--- src/install.sh
+++ src/install.sh
@@ -6,3 +6,4 @@ sudo insmod ./mydriver.ko
 sudo mknod /dev/queue0 c 238 0
 sudo echo "test" > /dev/queue0
 sudo cat /dev/queue0
+dmesg | grep 'My Driver:'
--- src/mydriver.c
+++ src/mydriver.c
@@ -3,8 +3,8 @@
 #include <linux/init.h>
 #include <linux/kernel.h>	/* printk() */
 #include <linux/slab.h>		/* kmalloc() */
-#include <linux/fs.h>		/* everything... */
-#include <linux/errno.h>	/* error codes */
+#include <linux/fs.h>
+#include <linux/errno.h>	/* Error Codes */
 #include <linux/types.h>	/* size_t */
 #include <linux/proc_fs.h>
 #include <linux/fcntl.h>	/* O_ACCMODE */
@@ -13,13 +13,12 @@
 #include <asm/switch_to.h>		/* cli(), *_flags */
 #include <asm/uaccess.h>	/* copy_*_user */
 #include <linux/uaccess.h>
-
 #include "mydriver_ioctl.h"
 
 #define DRIVER_MAJOR 0
-#define DRIVER_NR_DEVS 4 ///< number of devices
-#define CLASS_NAME "queue"        ///< /dev/CLASS_NAME/
-#define  DEVICE_NAME "0"    ///< /dev/CLASS_NAME/DEVICE_NAME , might be redundant  
+#define DRIVER_NR_DEVS 4
+#define CLASS_NAME "queue"
+#define MAX_LIMIT 32
 
 int driver_major = DRIVER_MAJOR;
 int driver_minor = 0;
@@ -31,7 +30,7 @@ module_param(driver_nr_devs, int, S_IRUGO);
 
 MODULE_AUTHOR("Furkan Cakir, Hakan Eroztekin, Orcun Ozdemir");
 MODULE_LICENSE("GPL v3");
-MODULE_DESCRIPTION("A simple character device which uses queue to read and write operations.");
+MODULE_DESCRIPTION("A simple in-memory character device driver which uses queue to read/write operations.");
 MODULE_VERSION("1.0");
 
 struct driver_dev {
@@ -45,92 +44,97 @@ struct driver_dev *driver_devices;
 
 int driver_open(struct inode *inode, struct file *filp)
 {
-	printk(KERN_INFO "My Driver: Device open is called.\n");
+	printk(KERN_INFO "My Driver: Open request is called.\n");
 
     struct driver_dev *dev;
 
     dev = container_of(inode->i_cdev, struct driver_dev, cdev);
     filp->private_data = dev;
 
-    /* trim the device if open was write-only */
     if ((filp->f_flags & O_ACCMODE) == O_WRONLY) {
-		printk(KERN_INFO "My Driver: Open request is write only.\n");
-        if (down_interruptible(&dev->sem)) {
-            printk(KERN_INFO "My Driver: Device is not interruptable.\n");
+		printk(KERN_INFO "My Driver: Open request is for write only mode.\n");
+        if (down_interruptible(&dev->sem)) {    // ->sem for calling semaphores and handling interrupts.
+            printk(KERN_INFO "My Driver: Not interruptable.\n");
             return -ERESTARTSYS;
         }
 
+    // If data is not NULL, so free all of previous data.
     if (dev->data) {
         kfree(dev->data);
+        dev->data = NULL;
+        dev->size = 0;
     }
-    dev->data = NULL;
-    dev->size = 0;
-        up(&dev->sem);
+
+        up(&dev->sem);  // Restore the semaphore value.
     }
-	printk(KERN_INFO "My Driver: Device opened successfully.\n");
+
+	printk(KERN_INFO "My Driver: Opened successfully.\n");
     return 0;
 }
 
 int driver_release(struct inode *inode, struct file *filp)
 {
-	printk(KERN_INFO "My Driver: Device is released.\n");
+	printk(KERN_INFO "My Driver: Released.\n");
     return 0;
 }
 
-/*
- *  @param filp A pointer to a file object
- *  @param buf The buffer to that contains the string to write to the device
- *  @param count The length of the array of data that is being passed in the const char buffer
- *  @param f_pos is the offset if required
- */
+ /***************************************************************************************
+ *  filp: A pointer to a file object.
+ *  buf: The buffer to that contains the string to write to the device.
+ *  count: The length of the array of data that is being passed in the const char buffer.
+ *  f_pos: Offset.
+ ****************************************************************************************/
 
-ssize_t driver_read(struct file *filp, char __user *buf, size_t count, 
-                   loff_t *f_pos)
+ssize_t driver_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
  {
-    printk(KERN_INFO "My Driver: Device read is going to be executed.\n");
-    struct driver_dev *dev = filp->private_data;
+    printk(KERN_INFO "My Driver: Read request is called.\n");
+    struct driver_dev *dev = filp->private_data;    // Private data in kernel for individual devices.
 
-    ssize_t bytes = count < (64-(*f_pos)) ? count : (64-(*f_pos));
+    ssize_t bytes = count < (MAX_LIMIT-(*f_pos)) ? count : (MAX_LIMIT-(*f_pos));
 
+/*
     if (down_interruptible(&dev->sem)) {
-        printk(KERN_INFO "My Driver: Device is not interruptable.\n");
+        printk(KERN_INFO "My Driver: Not interruptable.\n");
         return -ERESTARTSYS;
     }
+*/
 
-    if(copy_to_user(buf, dev->data, bytes)){
+    if (copy_to_user(buf, dev->data, bytes)) {
+        printk(KERN_INFO "My Driver: Copy data from kernel to user fail.\n");
         return -EFAULT;
     }
 
-    (*f_pos) += bytes;
+    (*f_pos) += bytes;  // Increment position iterator to synchronize buffer outputs.
 
-    up(&dev->sem);
-    return bytes;
+    up(&dev->sem);      // Send up signal to semaphore.
+    printk(KERN_INFO "My Driver: Returning from the write request.\n");
+    return bytes;       // Return only the amount of bytes that you copy to user to prevent errors and bugs.
 }
 
-/*
- *  @param filp A pointer to a file object
- *  @param buf The buffer to that contains the string to write to the device
- *  @param count The length of the array of data that is being passed in the const char buffer
- *  @param f_pos is the offset if required
- */
-
-ssize_t driver_write(struct file *filp, const char __user *buf, size_t count,
-                    loff_t *f_pos) // Send data to the device
+ssize_t driver_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
 {
+    printk(KERN_INFO "My Driver: Write request is called.\n");
     struct driver_dev *dev = filp->private_data;
-    printk(KERN_INFO "%d", count);
-    printk(KERN_INFO "%s", buf);
-    if (!dev->data) {
-        dev->data = kmalloc(50 * sizeof(char *), GFP_KERNEL);
-        if (!dev->data)
-            printk(KERN_INFO "lel");
-        memset(dev->data, 0, 50 * sizeof(char *));
+
+    printk(KERN_INFO "My Driver: Written data is '%s'", buf);
+    printk(KERN_INFO "My Driver: %d bytes were written.", count);
+
+    if (down_interruptible(&dev->sem)) {
+        printk(KERN_INFO "My Driver: Not interruptable.\n");
+        return -ERESTARTSYS;
     }
+
+    if (!dev->data) {
+        dev->data = kmalloc(count * sizeof(char *), GFP_KERNEL);
+        memset(dev->data, 0, count * sizeof(char *));
+    }
+
     copy_from_user(dev->data, buf, count);
     *f_pos += count;
     dev->size = *f_pos;
-    printk(KERN_INFO "%d", *f_pos);
+
     up(&dev->sem);
+    printk(KERN_INFO "My Driver: Returning from the read request.\n");
     return count;
     /*
     struct driver_dev *dev = filp->private_data;
@@ -232,9 +236,6 @@ loff_t driver_llseek(struct file *filp, loff_t off, int whence) // Change curren
     return newpos;
 }
 
-
-// location of the following part should not be changed. 
-// the functions had to be defined before this part
 struct file_operations driver_fops = { 
     .owner =    THIS_MODULE,
     .llseek =   driver_llseek,
@@ -247,9 +248,10 @@ struct file_operations driver_fops = {
 
 void driver_cleanup_module(void)
 {
-	printk(KERN_INFO "My Driver: Cleaning up the LKM.\n");
-    int i;
+	printk(KERN_INFO "My Driver: RMMOD request is called, cleaning up the module...\n");
+    
     dev_t devno = MKDEV(driver_major, driver_minor);
+    int i;
 
     if (driver_devices) {
         for (i = 0; i < driver_nr_devs; i++) {
@@ -259,12 +261,13 @@ void driver_cleanup_module(void)
     }
 
     unregister_chrdev_region(devno, driver_nr_devs);
-    printk(KERN_INFO "My Driver: Goodbye from the LKM!\n");
+    printk(KERN_INFO "My Driver: Cleanup was completed, goodbye from the module!\n");
+    return;
 }
 
 int driver_init_module(void)
 {
-	printk(KERN_INFO "My Driver: Initializing the LKM...\n");
+	printk(KERN_INFO "My Driver: Initializing the module...\n");
     
     int result, i;
     int err;
@@ -275,8 +278,7 @@ int driver_init_module(void)
         devno = MKDEV(driver_major, driver_minor);
         result = register_chrdev_region(devno, driver_nr_devs, "driver");
     } else {
-        result = alloc_chrdev_region(&devno, driver_minor, driver_nr_devs,
-                                     "driver");
+        result = alloc_chrdev_region(&devno, driver_minor, driver_nr_devs, "driver");
         driver_major = MAJOR(devno);
     }
     if (result < 0) {
@@ -286,8 +288,8 @@ int driver_init_module(void)
 
     printk(KERN_INFO "My Driver: Registered correctly with major number %d.\n", driver_major);
     
-    driver_devices = kmalloc(driver_nr_devs * sizeof(struct driver_dev),
-                            GFP_KERNEL); // allocate memory
+    driver_devices = kmalloc(driver_nr_devs * sizeof(struct driver_dev), GFP_KERNEL);
+
     if (!driver_devices) {
         result = -ENOMEM;
         printk(KERN_ALERT "My Driver: Failed to create the device.\n");
@@ -307,8 +309,9 @@ int driver_init_module(void)
         if (err)
             printk(KERN_NOTICE "My Driver: Error %d adding%d.\n", err, i);
     }
+
     printk(KERN_INFO "My Driver: Device initialized correctly.\n"); // device is initialized
-    return 0; /* succeed */
+    return 0;
 
   fail:
     driver_cleanup_module();
